945142a684578ae81441f13a929f622ea75a508f
unknown <lakshmisravani414@gmail.com>
2022-03-01 16:10:24 +0530

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,11 +8,11 @@ float b=20.0;
 System.out.println(a);
 System.out.println(b);
   System.out.println(c);
-<<<<<<< HEAD
+
 String Hello;
 System.out.println(String);
-=======
+
   int d=40;
->>>>>>> 167deb6d38224a0ebac4c74806399bdab607a581
+
 }
 }
