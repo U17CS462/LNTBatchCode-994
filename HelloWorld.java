6de0fae6bbb8b27b6dfa86e38b1a4731b5499bbb
U17CS462 <100669350+U17CS462@users.noreply.github.com>
2022-03-01 15:27:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,9 @@ public static void main(String[] args){
 System.out.println("Hello world from Sravani");
 int a=10;
 float b=20.0;
+  int c=30;
 System.out.println(a);
 System.out.println(b);
+  System.out.println(c);
 }
 }
